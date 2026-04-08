3aaeb1e29fdfc5f1514d4dca71d6b80e7a59f3cc
tilly <till.aust@uni-konstanz.de>
2026-04-08 09:45:54 +0900

    running learning curves on computer

---
+++ src/Interpretability/_run_learning_curves_ubuntu.sh
@@ -0,0 +1,12 @@
+grans=("1d" "1h" "2h" "4h" "6h" "8h" "12h" "15min" "30min")
+m1s=("Kronos" "Fincast")
+
+for gran in "${grans[@]}"; do
+    for m1 in "${m1s[@]}"; do
+        python learning_curves.py \
+              --model1 $m1 \
+              --granularity $gran \
+              --output_root "/home/till/PycharmProjects/Secondary-Model/src/Output"
+    done
+done
+
